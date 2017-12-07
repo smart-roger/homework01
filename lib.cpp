aee94afce158c04143f36e343230767e20caf4d0
smart-roger <__shurik@list.ru>
2017-12-07 20:42:58 +0800

    Restart project without open password

---
+++ lib.cpp
@@ -0,0 +1,6 @@
+#include "version.h"
+
+#include "lib.h"
+
+int version()
+{return PROJECT_VERSION_PATCH;}
---
+++ main.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+#include "version.h"
+
+//#include <spdlog/spdlog.h>
+
+int main(int argc, char* argv[]){
+	//auto logger = spdlogger::stdout_logger_mt("console");
+	//logger->info("version {} was started!", version());
+	
+	std::cout << "Hello, world!" << std::endl;
+	
+	return 0;
+}
+
